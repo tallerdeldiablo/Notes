8132d782f8c61de483612f58977ddae13c5f0398
rico <rikardoan@gmail.com>
2022-01-27 23:04:32 -0500

    deleted some extra  files

--- routes/notes.js
+++ routes/notes.js
@@ -1,38 +1,64 @@
-const fb = require('express').Router();
+const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('../helpers/fsUtils');
 
-// GET Route for retrieving all the feedback
-fb.get('/', (req, res) =>
-  readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)))
-);
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+  readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+});
 
-// POST Route for submitting feedback
-fb.post('/', (req, res) => {
-  // Destructuring assignment for the items in req.body
-  const { email, feedbackType, feedback } = req.body;
+// GET Route for a specific note
+notes.get('/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/notes.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.note_id === noteId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json('No note with that ID');
+    });
+});
 
-  // If all the required properties are present
-  if (email && feedbackType && feedback) {
-    // Variable for the object we will save
-    const newFeedback = {
-      email,
-      feedbackType,
-      feedback,
-      feedback_id: uuidv4(),
+// DELETE Route for a specific note
+notes.delete('/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/notes.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all notes except the one with the ID provided in the URL
+      const result = json.filter((note) => note.note_id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/notes.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
+});
+
+// POST Route for a new UX/UI note
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
     };
 
-    readAndAppend(newFeedback, './db/notes.json');
-
-    const response = {
-      status: 'success',
-      body: newFeedback,
-    };
-
-    res.json(response);
+    readAndAppend(newNote, './db/notes.json');
+    res.json(`Tip added successfully 🚀`);
   } else {
-    res.json('Error in posting feedback');
+    res.error('Error in adding tip');
   }
 });
 
-module.exports = fb;
+module.exports = notes;
